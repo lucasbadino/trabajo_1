e41b78238968f45c2a018fb3713c5f756ad72b04
Lucas Badino <lucas.badino.7@gmail.com>
2022-08-06 17:02:18 -0300

    search

--- coderstore/views.py
+++ coderstore/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-
-
+from alimentos.models import Productos
+from bebidas.models import Bebidas
+from panificacion.models import Panes
 
 def inicio(request):
     return render(request, "inicio.html", context={})
+
+
+def buscar_productos(request):
+    search = request.GET['search']
+    carne = Productos.object.filter(name__icontains = search)
+    bebida = Bebidas.object.filter(name__icontains = search)
+    panes = Panes.object.filter(name__icontains = search)
+    context = {	
+        'carnes': carne,
+        'bebidas': bebida,
+        'panes': panes
+                }
+    return render(request, 'search_product.html', context = context)
 
