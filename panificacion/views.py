ab296bccab64f8c0230ac6ba1bf59d7c6aee92bd
Lucas Badino <lucas.badino.7@gmail.com>
2022-08-06 13:11:13 -0300

    styles

--- panificacion/views.py
+++ panificacion/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+def panes(request):
+    rand = randint(0, 2000)
+    al1 = Productos.objects.create(name = 'Carne', description = 'Carne de ternera', sku = rand, price = 2000)
+    context = {
+        'carne': al1
+    }
+    return render(request, 'alimentos.html', context=context)
+
+
+def lista_panes(request):
+    
+    all = Productos.objects.all() 
+     
+    context ={
+        'lista' : all
+    }
+    return render(request,'alimentos.html', context = context)
+
